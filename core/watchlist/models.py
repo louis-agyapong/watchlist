92ff3a2a6b1239ab277037f012057468e8e4d70a
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-11-10 13:16:17 +0000

    model refactor

--- core/watchlist/models.py
+++ core/watchlist/models.py
@@ -3,43 +3,28 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-class Person(models.Model):
-    """
-    An individual
-    """
-
-    first_name = models.CharField(_("First Name"), max_length=255)
-    last_name = models.CharField(_("Last Name"), max_length=255)
-
-    def __str__(self) -> str:
-        return f"{self.first_name} + {self.last_name}"
-
-
-class Genre(models.Model):
-    """
-    Movie Genre
-    """
-
-    name = models.CharField(_("Name"), max_length=255)
+class StreamingPlatform(models.Model):
+    name = models.CharField(_("Name"), max_length=100)
+    about = models.TextField(_("About"), blank=True)
+    website = models.URLField(_("Website"), max_length=100)
 
     def __str__(self) -> str:
         return self.name
 
 
 class Movie(models.Model):
-    class MovieType(models.TextChoices):
-        MOVIE: str = "movie", _("MOVIE")
-        SERIES: str = "series", _("SERIES")
-        EPISODE: str = "EPISODE", _("EPISODE")
-
-    title = models.CharField(_("Title"), max_length=50)
-    release_year = models.DateField(_("Release Year"), blank=True, null=True)
-    type = models.CharField(_("Type"), max_length=50)
-    genre = models.ManyToManyField(Genre, verbose_name=_("Genre"), blank=True)
-    description = models.TextField(_("Description"), blank=True, default="")
+    title = models.CharField(_("Title"), max_length=100)
+    platform = models.ForeignKey(
+        StreamingPlatform,
+        verbose_name=_("Platform"),
+        on_delete=models.SET_NULL,
+        related_name="movies",
+        blank=True,
+        null=True,
+    )
+    plot = models.TextField(_("Story Line"), blank=True)
     active = models.BooleanField(_("Active"), default=True)
     created = models.DateTimeField(_("Created"), auto_now_add=True)
-    updated = models.DateTimeField(_("Updated"), auto_now=True)
 
     def __str__(self) -> str:
-        return self.name
+        return self.title
