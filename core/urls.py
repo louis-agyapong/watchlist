5d40dc8a28337ae28b88115b197a668028839559
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-11-10 13:17:01 +0000

    urls refactor

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("movie/", include("core.watchlist.urls")),
-    # path("api/movie/", include("core.watchlist.api.urls"))
+    path("movie/", include("core.watchlist.urls")),
+    path("api/movie/", include("core.watchlist.api.urls"))
 ]
