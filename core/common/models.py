5123a4e24153094c66a1ae363a079de95aee61ca
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-11-11 22:19:31 +0000

    Soft delete model

--- core/common/models.py
+++ core/common/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from .managers import SoftDeleteManager
 
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(_("Create at"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class SoftDeleteModel(models.Model):
+    is_deleted = models.BooleanField(_("Is Deleted"), default=True)
+    objects = SoftDeleteManager()
+
 
     class Meta:
         abstract = True
