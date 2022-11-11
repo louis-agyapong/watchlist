13551c741a3128860a828b6ec24e2e4f60a83755
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-11-11 22:01:09 +0000

    Base Model: created_at, updated_at

--- core/common/models.py
+++ core/common/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(_("Create at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+
+    class Meta:
+        abstract = True
