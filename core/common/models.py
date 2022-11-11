502a2fcac1c4628553898c1f50c1143411e670ae
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-11-11 22:59:31 +0000

    soft delete

--- core/common/models.py
+++ core/common/models.py
@@ -1,5 +1,7 @@
+from django.core.checks.messages import Error
 from django.db import models
 from django.utils.translation import gettext as _
+
 from .managers import SoftDeleteManager
 
 
@@ -14,7 +16,18 @@ class BaseModel(models.Model):
 class SoftDeleteModel(models.Model):
     is_deleted = models.BooleanField(_("Is Deleted"), default=True)
     objects = SoftDeleteManager()
+    all_objects = models.Manager()  # default manager
 
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
+
+    def restore_delete(self):
+        self.is_deleted = False
+        self.save()
+
+    def delete(self):
+        raise Error()
 
     class Meta:
         abstract = True
