c04038856078b42343eb7d5c6076841ad99a0826
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-11-11 21:57:31 +0000

    misc.

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Local apps
     "core.watchlist",
+    "core.common",
     # External apps
     "rest_framework",
     "django_extensions",
